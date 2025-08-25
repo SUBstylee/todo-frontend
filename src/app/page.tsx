d8b7a9ae70af2a1b4c1f3670523cc4f946cd7c23
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-25 21:50:10 +0800

    started on header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,17 @@
 import { useState, useEffect } from 'react';
 import { Task } from '../types/types';
 
+import Header from '@/components/Header';
+
 export default function Home() {
 	const [tasks, setTasks] = useState<Task[]>([]);
 
 	useEffect(() => {
 		console.log('useEffect');
 	}, []);
-	return <div className='min-h-screen bg-[#1a1a1a] text-white'>page</div>;
+	return (
+		<div className='min-h-screen bg-[#1a1a1a] text-white'>
+			<Header />
+		</div>
+	);
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+
+const Header = () => {
+	return (
+		<header
+			className='w-full flex flex-col items-center p-8'
+			style={{ backgroundColor: '#0d0d0d' }}>
+			Header
+		</header>
+	);
+};
+
+export default Header;
