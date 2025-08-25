48f5bb039d26144415d5fb32ba122a3ff60b536c
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-25 22:43:11 +0800

    style changes

--- src/components/ButtonCreateTask.tsx
+++ src/components/ButtonCreateTask.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 const ButtonCreateTask = () => {
 	return (
 		<Link href='/create'>
-			<button className='w-full bg-blue-500 text-white rounded-md hover:bg-blue-400 px-6 py-3 transition flex items-center justify-center gap-2 -mt-6'>
+			<button className='w-full bg-blue-500 text-white cursor-pointer rounded-md hover:bg-blue-600 px-6 py-3 transition flex items-center justify-center gap-2 -mt-6'>
 				<span>Create Task</span>
 				<Image
 					src='/addtask.svg'
