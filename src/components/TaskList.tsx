177b46b2a9ae45fe9716e0b22a9346901a60be3c
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-25 22:54:23 +0800

    added hr stylized to match figma

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 const TaskList = () => {
 	return (
 		<div className='flex flex-col items-center text-center'>
+			<hr className='w-full h-[15px] border-t border-solid border-gray-500 rounded-full mb-5' />
 			<Image src='/clipboard.png' height={60} width={60} alt='Clipboard' />
 			<p className='text-lg font-medium mt-6'>
 				You don’t have any tasks registered yet.
