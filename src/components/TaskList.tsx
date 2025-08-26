25f93364fdaca5c28de0d17ae874ed5d4f1f433f
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-26 18:09:24 +0800

    building out tasklist view when tasks exist

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,16 +1,21 @@
 import Image from 'next/image';
+import { TaskListProps } from '@/types/types';
 
-const TaskList = () => {
+const TaskList = ({ tasks }: TaskListProps) => {
 	return (
-		<div className='flex flex-col items-center text-center'>
-			<hr className='w-full h-[15px] border-t border-solid border-gray-500 rounded-full mb-5' />
-			<Image src='/clipboard.png' height={60} width={60} alt='Clipboard' />
-			<p className='text-lg font-medium mt-6'>
-				You don’t have any tasks registered yet.
-			</p>
-			<p className='text-sm text-gray-500'>
-				Create tasks and organize your to-do items.
-			</p>
+		<div>
+			{tasks.length ? (
+				<div>there are tasks</div>
+			) : (
+				<div className='flex flex-col items-center text-center'>
+					<hr className='w-full h-[15px] border-t border-solid border-gray-500 rounded-full mb-5' />
+					<Image src='/clipboard.png' height={60} width={60} alt='Clipboard' />
+					<p className='mt-6'>You don&apos;t have any tasks registered yet.</p>
+					<p className='text-gray-500 mt-6'>
+						Create tasks and organize your to-do items.
+					</p>
+				</div>
+			)}
 		</div>
 	);
 };
