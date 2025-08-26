9754b7af250640e68052d5c2a8c376d23fb6a222
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-26 20:20:33 +0800

    more button adjustments

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image';
 
 const BackButton: React.FC = () => {
 	const router = useRouter();
-	// decided to go with this check for scalability. as the app grows, might not want to always bring them back to root. could also later add a prop to go somewhere specific and adjust the logic here.
 	const handleBackClick = () =>
 		window.history.length > 1 ? router.back() : router.push('/');
 
