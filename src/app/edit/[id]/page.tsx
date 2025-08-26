7e8e5955edfb35fbf529e44185952179191b7518
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-26 22:52:30 +0800

    text to copy.json

--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { SetStateAction, useState } from 'react';
+import { useState } from 'react';
 
 import BackButton from '@/components/BackButton';
 import Header from '@/components/Header';
@@ -7,19 +7,24 @@ import Input from '@/components/Input';
 import AppButton from '@/components/AppButton';
 import ColorCode from '@/components/ColorCode';
 
+import copy from '@/data/copy.json';
+
 const EditTaskPage = () => {
 	const [title, setTitle] = useState('');
 	const [selectedColor, setSelectedColor] = useState<string>('#fff');
 
 	return (
-		<div className='min-h-screen bg-[#1a1a1a] text-white flex flex-col items-center'>
+		<div
+			className={
+				'min-h-screen bg-[#1a1a1a] text-white flex flex-col items-center'
+			}>
 			<Header />
-			<main className='w-full max-w-2xl flex flex-col  mt-12 px-4 sm:px-0'>
+			<main className={'w-full max-w-2xl flex flex-col  mt-12 px-4 sm:px-0'}>
 				<BackButton />
 				<Input
 					title={title}
 					setTitle={setTitle}
-					label={'Title'}
+					label={copy.editPage.input.label}
 					placeHolder={''}
 				/>
 
@@ -29,13 +34,13 @@ const EditTaskPage = () => {
 				/>
 
 				<AppButton
-					textContent={'Save'}
+					textContent={copy.editPage.appButton.textContent}
 					handleClick={() => console.log('saved, but not really')}
-					imgSrc='/checkmark.svg'
+					imgSrc={'/checkmark.svg'}
 					width={20}
 					height={20}
-					imgClassName='inline-block'
-					btnClassName='mt-8'
+					imgClassName={'inline-block'}
+					btnClassName={'mt-8'}
 				/>
 			</main>
 		</div>
