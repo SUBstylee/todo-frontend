08a14de878373bd21327c72ebe3e617fb51520d5
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-26 18:08:08 +0800

    mock task data, used for stub data, can also be used for testing

---
+++ src/constants/stub.ts
@@ -0,0 +1,22 @@
+import { Task } from '@/types/types';
+
+export const mockTasks: Task[] = [
+	{
+		id: 1,
+		title: 'Buy groceries',
+		completedStatus: false,
+		color: 'green',
+	},
+	{
+		id: 2,
+		title: 'Finish TypeScript project',
+		completedStatus: true,
+		color: 'blue',
+	},
+	{
+		id: 3,
+		title: 'Call mom',
+		completedStatus: false,
+		color: 'red',
+	},
+];
