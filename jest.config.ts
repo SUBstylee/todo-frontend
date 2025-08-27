572ff232bb2fe9c2b5f39c8a717c0ce434099090
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-28 01:28:08 +0800

    fixed issues with running tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,20 @@
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-	preset: 'ts-jest',
+	preset: 'ts-jest/presets/js-with-ts-esm', // better for Next.js + TSX
 	testEnvironment: 'jsdom',
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
 	transform: {
-		'^.+\\.(ts|tsx)$': 'ts-jest',
+		'^.+\\.(ts|tsx)$': [
+			'ts-jest',
+			{ tsconfig: './tsconfig.jest.json', useESM: true },
+		],
 	},
 	moduleNameMapper: {
 		'^@/(.*)$': '<rootDir>/src/$1',
 		'\\.(css|scss|sass|svg)$': '<rootDir>/__mocks__/fileMock.js',
 	},
+	extensionsToTreatAsEsm: ['.ts', '.tsx'], // ensures tsx files are treated as ESM
 	testPathIgnorePatterns: ['/node_modules/', '/.next/'],
 };
 
