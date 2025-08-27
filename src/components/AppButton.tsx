4ae5041133ba0d8bde2494fccae4daf09cb2912f
Jeremy Threlfall <jjthrelfall@gmail.com>
2025-08-27 18:21:55 +0800

    accessibility enhancements

--- src/components/AppButton.tsx
+++ src/components/AppButton.tsx
@@ -2,24 +2,28 @@ import Image from 'next/image';
 import { ButtonProps } from '../types/types';
 
 const AppButton = ({
-	imgSrc,
-	alt,
+	imgSrc = '',
+	alt = '',
+	ariaLabel = '',
 	width,
 	height,
-	imgClassName,
-	btnClassName,
+	imgClassName = '',
+	btnClassName = '',
 	textContent,
-	handleClick,
+	handleClick = () => {},
 }: ButtonProps) => {
+	const showImg = imgSrc && (height || width);
+
 	return (
 		<button
 			onClick={handleClick}
 			className={`w-full bg-blue-500 text-white cursor-pointer rounded-md hover:bg-blue-600 px-6 py-3 transition flex items-center justify-center gap-2 ${btnClassName}`}>
 			<span>{textContent}</span>
-			{imgSrc && (
+			{showImg && (
 				<Image
 					src={imgSrc}
-					alt={alt || ''}
+					alt={alt}
+					aria-label={ariaLabel}
 					width={width}
 					height={height}
 					className={imgClassName}
